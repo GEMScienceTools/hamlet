dcb267f8cc51c6de9da0f861395c1907e004a24a
itchy <richard.h.styron@gmail.com>
2019-06-12 14:47:32 -0700

    changed up relm stats

---
+++ hztest/model_test_frameworks/relm/relm_stats.py
@@ -0,0 +1,28 @@
+import numpy as np
+from hztest.utils.stats import binomial_likelihood, binomial_log_likelihood
+
+def bin_observance_likelihood(num_events: int, bin_rate: float) -> float:
+    not_modeled_val = 0. # hardcoded in the RELM tests
+
+    return binomial_likelihood(bin_rate, num_events, not_modeled_val)
+
+
+def bin_observance_log_likelihood(num_events: int, bin_rate: float) -> float:
+    if bin_rate == 0:
+        return bin_observance_log_likelihood_zero_rate(num_events)
+    else:
+        return (-1 * bin_rate + num_events * np.log(bin_rate) 
+                - np.log(np.math.factorial(bin_rate)))
+
+
+def bin_observance_log_likelihood_zero_rate(num_events: int) -> float:
+    if num_events == 0:
+        return np.log(1)
+    elif num_events > 0:
+        return -1 * np.infty
+    else:
+        raise ValueError
+
+
+def model_log_likelihood(bin_event_counts, bin_rates):
+    return np.sum(bin_observance_log_likelihood(bin_event_counts, bin_rates))
--- hztest/utils/stats.py
+++ hztest/utils/stats.py
@@ -1,8 +1,16 @@
+from typing import Optional
+
 import numpy as np
 
 def sample_event_times_in_interval(annual_occurrence_rate: float, 
                                    interval_length: float, 
-                                   t0: float=0., rand_seed=None):
+                                   t0: float=0., 
+                                   rand_seed: Optional[int]=None
+                                   ) -> np.ndarray:
+
+    """
+
+    """
 
 
     if rand_seed is not None:
@@ -15,38 +23,22 @@ def sample_event_times_in_interval(annual_occurrence_rate: float,
     return event_times
 
 
-def bin_observance_likelihood(num_events: int, bin_rate: float) -> float:
-    if bin_rate == 0:
-        return bin_observance_likelihood_zero_rate(num_events)
+def binomial_likelihood(rate: float, num_events: int,
+                        not_modeled_val: float=0.) -> float:
+    if rate == 0:
+        pass
     else:
-        return bin_rate / np.math.factorial(num_events) * np.exp(bin_rate)
+        return rate / np.math.factorial(num_events) * np.exp(rate)
 
 
-def bin_observance_log_likelihood(num_events: int, bin_rate: float) -> float:
-    if bin_rate == 0:
-        return bin_observance_log_likelihood_zero_rate(num_events)
-    else:
-        return (-1 * bin_rate + num_events * np.log(bin_rate) 
-                - np.log(np.math.factorial(bin_rate)))
-
-
-def bin_observance_likelihood_zero_rate(num_events: int) -> float:
+def binomial_likelihood_zero_rate(num_events: int, 
+                                  not_modeled_val: float=0.) -> float:
     if num_events == 0:
-        return 1
+        return 1.
     elif num_events > 0:
-        return 0
+        return not_modeled_val
     else:
-        raise ValueError
+        raise ValueError("num_events should be zero or a positive integer.")
 
-
-def bin_observance_log_likelihood_zero_rate(num_events: int) -> float:
-    if num_events == 0:
-        return np.log(1)
-    elif num_events > 0:
-        return -1 * np.infty
-    else:
-        raise ValueError
-
-
-def model_log_likelihood(bin_event_counts, bin_rates):
-    return np.sum(bin_observance_log_likelihood(bin_event_counts, bin_rates))
+def binomial_log_likelihood():
+    raise NotImplementedError
