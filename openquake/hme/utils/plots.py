b0acfa5136630b237a3cb32345eef2df6752be5f
Richard Styron <richard.h.styron@gmail.com>
2019-10-31 12:31:05 -0700

    likelihood map and reporting

--- openquake/hme/utils/plots.py
+++ openquake/hme/utils/plots.py
@@ -2,6 +2,8 @@ import io
 from typing import Union, Optional
 
 import matplotlib.pyplot as plt
+import geopandas as gpd
+from geopandas import GeoDataFrame
 
 
 def plot_mfd(model: Optional[dict] = None,
@@ -51,3 +53,38 @@ def plot_mfd(model: Optional[dict] = None,
         plt.close(fig)
         fig_svg = '<svg' + fig_str.getvalue().split('<svg')[1]
         return fig_svg
+
+
+def plot_likelihood_map(bin_gdf: GeoDataFrame,
+                        eq_gdf: Optional[GeoDataFrame] = None):
+
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+    bin_gdf.plot(column='log_like',
+                 ax=ax,
+                 vmin=0.,
+                 vmax=1.,
+                 cmap='OrRd_r',
+                 legend=True)
+
+    x_lims = ax.get_xlim()
+    y_lims = ax.get_ylim()
+
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    world.plot(ax=ax, color='none', edgecolor='black')
+
+    if eq_gdf is not None:
+        ax.scatter(eq_gdf.longitude,
+                   eq_gdf.latitude,
+                   s=(eq_gdf.magnitude**3) / 10.,
+                   edgecolor='blue',
+                   facecolors='none')
+
+    ax.set_xlim(x_lims)
+    ax.set_ylim(y_lims)
+
+    plt.switch_backend('svg')
+    fig_str = io.StringIO()
+    fig.savefig(fig_str, format='svg')
+    fig_svg = '<svg' + fig_str.getvalue().split('<svg')[1]
+    return fig_svg
--- openquake/hme/utils/reporting/reporting.py
+++ openquake/hme/utils/reporting/reporting.py
@@ -1,8 +1,12 @@
 import os
 from typing import Optional
 
-from jinja2 import Environment, FileSystemLoader
+import numpy as np
+import matplotlib.pyplot as plt
 from geopandas import GeoDataFrame
+from jinja2 import Environment, FileSystemLoader
+
+from openquake.hme.utils.plots import plot_likelihood_map
 
 BASE_DATA_PATH = os.path.dirname(__file__)
 template_dir = os.path.join(BASE_DATA_PATH, 'templates')
@@ -21,7 +25,11 @@ def generate_basic_report(cfg: dict,
     env = _init_env()
     report_template = env.get_template('basic_report.html')
 
-    render_result_text(env=env, cfg=cfg, results=results)
+    render_result_text(env=env,
+                       cfg=cfg,
+                       results=results,
+                       bin_gdf=bin_gdf,
+                       eq_gdf=eq_gdf)
 
     report = report_template.render(cfg=cfg, results=results)
 
@@ -58,6 +66,15 @@ def render_likelihood(env: Environment,
                       bin_gdf: GeoDataFrame,
                       eq_gdf: Optional[GeoDataFrame] = None):
 
+    total_log_like = np.sum(bin_gdf['log_like']) / bin_gdf.shape[0]
+    total_log_like = "{0:2f}".format(total_log_like)
+
+    likelihood_map_str = plot_likelihood_map(bin_gdf, eq_gdf)
+
     likelihood_template = env.get_template('likelihood.html')
+
     results['likelihood']['rendered_text'] = likelihood_template.render(
-        cfg=cfg, results=results)
+        cfg=cfg,
+        results=results,
+        total_log_like=total_log_like,
+        likelihood_map_str=likelihood_map_str)
