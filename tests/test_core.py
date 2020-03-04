3cd25abdfb425bd92952872eb792800344b23408
Richard Styron <richard.h.styron@gmail.com>
2020-03-04 15:51:51 -0800

    test_core fix

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,7 +2,6 @@ import os
 import unittest
 
 from openquake.hme.core.core import (read_yaml_config,
-                                     get_test_list_from_config,
                                      get_test_lists_from_config)
 
 yml_1 = './model_test_frameworks/gem/integration_tests/gem_sm1/test_sm1_poisson.yml'
@@ -10,8 +9,6 @@ yml_2 = './data/unit_test_data/test_core_1.yml'
 
 cfg_1 = read_yaml_config(yml_1)
 cfg_2 = read_yaml_config(yml_2)
-
-tests_1 = get_test_list_from_config(cfg_1)
 
 
 def test_get_test_lists_from_config():
