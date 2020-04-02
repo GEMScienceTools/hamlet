ec25c452dc96872039fd469272528ae209ad2780
itchy <richard.h.styron@gmail.com>
2020-04-02 07:23:14 -0700

    changed np.log to math.log for Poisson log-like

--- openquake/hme/utils/stats.py
+++ openquake/hme/utils/stats.py
@@ -2,18 +2,17 @@
 This module provides statistical functions that are not specific to any set of
 tests.
 """
+import math
 from typing import Optional, Union, Tuple
 
 import numpy as np
-from scipy.special import gamma, factorial
-from scipy.optimize import minimize
 
 
 def sample_event_times_in_interval(
-    annual_occurrence_rate: float,
-    interval_length: float,
-    t0: float = 0.0,
-    rand_seed: Optional[int] = None,
+        annual_occurrence_rate: float,
+        interval_length: float,
+        t0: float = 0.0,
+        rand_seed: Optional[int] = None,
 ) -> np.ndarray:
     """
     Returns the times of events 
@@ -25,15 +24,17 @@ def sample_event_times_in_interval(
 
     n_events = np.random.poisson(annual_occurrence_rate * interval_length)
 
-    event_times = np.random.uniform(low=t0, high=t0 + interval_length, size=n_events)
+    event_times = np.random.uniform(low=t0,
+                                    high=t0 + interval_length,
+                                    size=n_events)
     return event_times
 
 
 def poisson_likelihood(
-    num_events: int,
-    rate: float,
-    time_interval: float = 1.0,
-    not_modeled_val: float = 0.0,
+        num_events: int,
+        rate: float,
+        time_interval: float = 1.0,
+        not_modeled_val: float = 0.0,
 ) -> float:
     """
     Returns the Poisson likelihood of observing `num_events` in a
@@ -54,12 +55,11 @@ def poisson_likelihood(
         return poisson_likelihood_zero_rate(num_events, not_modeled_val)
     else:
         rt = rate * time_interval
-        return np.exp(-rt) * rt ** num_events / np.math.factorial(num_events)
+        return np.exp(-rt) * rt**num_events / np.math.factorial(num_events)
 
 
-def poisson_likelihood_zero_rate(
-    num_events: int, not_modeled_val: float = 0.0
-) -> float:
+def poisson_likelihood_zero_rate(num_events: int,
+                                 not_modeled_val: float = 0.0) -> float:
     if num_events == 0:
         return 1.0
     elif num_events > 0:
@@ -69,46 +69,46 @@ def poisson_likelihood_zero_rate(
 
 
 def poisson_log_likelihood(
-    num_events: int,
-    rate: float,
-    time_interval: float = 1.0,
-    not_modeled_val: float = 0.0,
+        num_events: int,
+        rate: float,
+        time_interval: float = 1.0,
+        not_modeled_val: float = 0.0,
 ) -> float:
 
     if rate == 0.0:
         return np.log(poisson_likelihood_zero_rate(rate, not_modeled_val))
     else:
         rt = rate * time_interval
-        return -1 * rt + num_events * np.log(rt) - np.log(np.math.factorial(num_events))
+        return (-1 * rt + num_events * math.log(rt) -
+                math.log(np.math.factorial(num_events)))
 
 
-def negative_binomial_distribution(
-    num_events: int, prob_success: float, r_dispersion: Union[int, float]
-) -> float:
+def negative_binomial_distribution(num_events: int, prob_success: float,
+                                   r_dispersion: Union[int, float]) -> float:
     """
     Returns the negative binomial probability for observing 
 
     """
 
     term_1 = (gamma(num_events + r_dispersion)) / (
-        gamma(r_dispersion) * np.math.factorial(num_events)
-    )
+        gamma(r_dispersion) * np.math.factorial(num_events))
 
-    term_2 = (1 - prob_success) ** r_dispersion
+    term_2 = (1 - prob_success)**r_dispersion
 
-    term_3 = prob_success ** num_events
+    term_3 = prob_success**num_events
 
     return term_1 * term_2 * term_3
 
 
 def estimate_negative_binom_parameters(
-    samples, rate: Optional[float] = None, mle: bool = False
-) -> Tuple[float, float]:
+        samples,
+        rate: Optional[float] = None,
+        mle: bool = False) -> Tuple[float, float]:
     if mle is False:
         mean = rate or np.mean(samples)
         variance = np.var(samples, ddof=1)
         prob_success = (variance - mean) / variance
-        r_dispersion = mean ** 2 / (variance - mean)
+        r_dispersion = mean**2 / (variance - mean)
 
     else:
         # maximum likelihood estimation will go here
@@ -139,9 +139,8 @@ def jensen_shannon_divergence(p, q):
 
     r = _mid_pt_measure(pp, qq)
 
-    return 0.5 * (
-        kullback_leibler_divergence(pp, r) + kullback_leibler_divergence(qq, r)
-    )
+    return 0.5 * (kullback_leibler_divergence(pp, r) +
+                  kullback_leibler_divergence(qq, r))
 
 
 def jensen_shannon_distance(p, q):
