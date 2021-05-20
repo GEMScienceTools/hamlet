ed8fac946c0484017197d61a0c2579ad52c4c580
Richard Styron <richard.h.styron@gmail.com>
2021-05-20 07:13:06 -0700

    version bump to docs

--- doc_src/source/conf.py
+++ doc_src/source/conf.py
@@ -16,12 +16,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Hamlet'
-copyright = '2019-2020, Richard Styron (GEM Foundation)'
-author = 'Richard Styron (GEM Foundation)'
+project = "Hamlet"
+copyright = "2019-2020, Richard Styron (GEM Foundation)"
+author = "Richard Styron (GEM Foundation)"
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = "0.4.0"
 
 # -- General configuration ---------------------------------------------------
 
@@ -29,16 +29,16 @@ release = '0.2.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
     #'sphinx.ext.intersphinx',
-    'recommonmark',
-    'sphinx.ext.mathjax',
+    "recommonmark",
+    "sphinx.ext.mathjax",
 ]
 
-#intersphinx_mapping = {''}
+# intersphinx_mapping = {''}
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -50,9 +50,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = "bizstyle"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
