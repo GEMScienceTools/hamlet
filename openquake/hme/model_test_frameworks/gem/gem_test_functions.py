fca4c74650b601c51e3f34ca297135602c380ef1
Richard Styron <richard.h.styron@gmail.com>
2022-06-14 15:46:43 -0700

    provisional moment over under fn

--- openquake/hme/model_test_frameworks/gem/gem_test_functions.py
+++ openquake/hme/model_test_frameworks/gem/gem_test_functions.py
@@ -11,9 +11,84 @@ from openquake.hme.utils import (
     SpacemagBin,
     parallelize,
     mag_to_mo,
+    sample_rups,
     get_model_mfd,
     get_obs_mfd,
 )
+
+
+def get_catalog_moment(eq_df, eq_groups=None):
+    if eq_groups == None:
+        eq_groups = eq_df.groupby("cell_id")
+
+    moment_sums = {
+        name: mag_to_mo(group["magnitude"]).sum() for name, group in eq_groups
+    }
+
+    total_sum = sum(moment_sums.values())
+
+    return moment_sums, total_sum
+
+
+def moment_over_under_eval_fn(
+    rup_df, eq_gdf, t_yrs, min_mag=1.0, max_mag=10.0, n_iters=1000
+):
+
+    cell_ids = sorted(rup_df.cell_id.unique())
+
+    cell_moment_iterations = {
+        cell_id: np.zeros(n_iters) for cell_id in cell_ids
+    }
+    # cell_moment_iterations = pd.DataFrame(index=cell_ids, columns=np.arange(n_iters),
+    #                                      data=np.zeros((len(cell_ids), n_iters)))
+    total_moment_iterations = np.zeros(n_iters)
+
+    for i in range(n_iters):
+        rup_sample = sample_rups(
+            rup_df, t_yrs, min_mag=min_mag, max_mag=max_mag
+        )
+        iter_moments, iter_moment_sum = get_catalog_moment(rup_sample)
+
+        # cell_moment_iterations[i].add(pd.Series(iter_moments), fill_value=0.)
+
+        for cell_id, moment_sum in iter_moments.items():
+            cell_moment_iterations[cell_id][i] += moment_sum
+
+        total_moment_iterations[i] += iter_moment_sum
+
+    cat_cell_moments, cat_total_moment = get_catalog_moment(eq_gdf)
+
+    cell_fracs = {
+        cell_id: sum(
+            cell_moment_iterations[cell_id] < cat_cell_moments.get(cell_id, 0.0)
+        )
+        / n_iters
+        for cell_id in cell_ids
+    }
+
+    total_frac = sum(total_moment_iterations < cat_total_moment) / n_iters
+
+    results = {
+        "test_data": {
+            "total_obs_moment": cat_total_moment,
+            "modeled_obs_moment": {
+                "mean": total_moment_iterations.mean(),
+                "sd": np.std(total_moment_iterations),
+            },
+            "frac": total_frac,
+            "cell_fracs": cell_fracs,
+            "stoch_total_moments": total_moment_iterations,
+            "stoch_cell_moments": iter_moments,
+            "obs_cell_moments": cat_cell_moments,
+        }
+    }
+
+    return results
+
+
+###
+# old
+###
 
 
 def get_mfd_freq_counts(eq_counts: Sequence[int]) -> Dict:
@@ -35,7 +110,8 @@ def get_mfd_freq_counts(eq_counts: Sequence[int]) -> Dict:
 
     n_eqs, n_occurrences = np.unique(eq_counts, return_counts=True)
     mfd_freq_counts = {
-        n_eq: n_occurrences[i] / sum(n_occurrences) for i, n_eq in enumerate(n_eqs)
+        n_eq: n_occurrences[i] / sum(n_occurrences)
+        for i, n_eq in enumerate(n_eqs)
     }
     return mfd_freq_counts
 
@@ -63,7 +139,9 @@ def get_stochastic_mfd_counts(
         number of occurrences (counts) of that magnitude for each iteration.
     """
 
-    mfd_counts: Dict[float, list] = {bc: [] for bc in spacemag_bin.mag_bin_centers}
+    mfd_counts: Dict[float, list] = {
+        bc: [] for bc in spacemag_bin.mag_bin_centers
+    }
 
     for i in range(n_iters):
         for bc, n_eqs in spacemag_bin.get_rupture_sample_mfd(
@@ -94,7 +172,9 @@ def get_stochastic_mfd(
     :returns: Dictionary with keys of earthquake magnitudes and values of a list
         of number of occurrences (counts) of that magnitude for each iteration.
     """
-    mfd_counts = get_stochastic_mfd_counts(spacemag_bin, n_iters, interval_length)
+    mfd_counts = get_stochastic_mfd_counts(
+        spacemag_bin, n_iters, interval_length
+    )
 
     mfd_freq_counts = {}
 
@@ -113,11 +193,17 @@ def get_stochastic_mfds_parallel(geo_series: GeoSeries, **kwargs):
 
 
 def get_stochastic_moment_set(
-    spacemag_bin: SpacemagBin, interval_length: float, n_iters: int, rand_seed=None
+    spacemag_bin: SpacemagBin,
+    interval_length: float,
+    n_iters: int,
+    rand_seed=None,
 ) -> np.ndarray:
     """"""
     return np.array(
-        [get_stochastic_moment(spacemag_bin, interval_length) for i in range(n_iters)]
+        [
+            get_stochastic_moment(spacemag_bin, interval_length)
+            for i in range(n_iters)
+        ]
     )
 
 
@@ -203,11 +289,17 @@ def eval_obs_moment(
     )
 
     obs_moment_rank = rank_obs_moment(
-        spacemag_bin, interval_length, n_iters, stoch_moment_sums=stoch_moment_sums
+        spacemag_bin,
+        interval_length,
+        n_iters,
+        stoch_moment_sums=stoch_moment_sums,
     )
 
     moment_ratio = obs_stoch_moment_ratio(
-        spacemag_bin, interval_length, n_iters, stoch_moment_sums=stoch_moment_sums
+        spacemag_bin,
+        interval_length,
+        n_iters,
+        stoch_moment_sums=stoch_moment_sums,
     )
 
     return {"obs_moment_rank": obs_moment_rank, "moment_ratio": moment_ratio}
@@ -240,7 +332,8 @@ def eval_obs_moment_model(spacemag_bins, interval_length: float, n_iters: int):
     obs_moment_sum = obs_moment_sums.sum()
 
     obs_moment_rank = (
-        len(model_stoch_moment_sums[model_stoch_moment_sums < obs_moment_sum]) / n_iters
+        len(model_stoch_moment_sums[model_stoch_moment_sums < obs_moment_sum])
+        / n_iters
     )
 
     if model_stoch_moment_mean == 0.0:
@@ -262,6 +355,8 @@ def eval_obs_moment_model(spacemag_bins, interval_length: float, n_iters: int):
 
 
 def get_moment_from_mfd(mfd: dict) -> float:
-    mo = sum(mag_to_mo(np.array(list(mfd.keys()))) * np.array(list(mfd.values())))
+    mo = sum(
+        mag_to_mo(np.array(list(mfd.keys()))) * np.array(list(mfd.values()))
+    )
 
     return mo
