06f90a26bc1974e83085736aacf18c2a8e9712f4
Richard Styron <richard.h.styron@gmail.com>
2023-02-21 17:00:33 -0800

    updated core tests (sparse as they are)

--- openquake/hme/core/tests/test_core.py
+++ openquake/hme/core/tests/test_core.py
@@ -3,9 +3,9 @@ import os
 from openquake.hme.core.core import read_yaml_config, get_test_lists_from_config
 
 BASE_PATH = os.path.dirname(__file__)
-UNIT_TEST_DATA_DIR = os.path.join(BASE_PATH, "data", "unit_test_data")
+# UNIT_TEST_DATA_DIR = os.path.join(BASE_PATH, "data", "unit_test_data")
 
-yml_2 = os.path.join(UNIT_TEST_DATA_DIR, "test_core_1.yml")
+yml_2 = os.path.join(BASE_PATH, "test_core_1.yml")
 
 cfg_2 = read_yaml_config(yml_2, validate=False)
 
@@ -15,20 +15,21 @@ def test_read_yaml_config():
     assert cfg == {
         "input": {
             "bins": {"h3_res": 3, "mfd_bin_max": 9.0},
-            "subset": {"file": None, "buffer": 0.0},
             "ssm": {
                 "branch": "b1",
                 "tectonic_region_types": ["Active Shallow Crust"],
                 "source_types": None,
+                "max_depth": None,
+                "job_ini_file": None,
                 "ssm_dir": "../../../../data/source_models/sm1/",
                 "ssm_lt_file": "ssmLT.xml",
-                "max_depth": None,
             },
             "rupture_file": {
                 "rupture_file_path": None,
                 "read_rupture_file": False,
                 "save_rupture_file": False,
             },
+            "subset": {"file": None, "buffer": 0.0},
             "simple_ruptures": True,
         },
         "meta": {"description": "Fake yaml for testing"},
@@ -43,8 +44,6 @@ def test_read_yaml_config():
 
 def test_get_test_lists_from_config():
     test_dict = get_test_lists_from_config(cfg_2)
-    gem_test_names = [f.__name__ for f in test_dict["gem"]]
-    sanity_test_names = [f.__name__ for f in test_dict["sanity"]]
 
-    assert gem_test_names == ["mfd_likelihood_test"]
-    assert sanity_test_names == ["max_check"]
+    assert test_dict["gem"] == ["likelihood"]
+    assert test_dict["sanity"] == ["max_check"]
