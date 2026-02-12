8e529d28fbdfd43a301741d2eb1ccb9c51fd8f96
Richard Styron <richard.h.styron@gmail.com>
2026-02-12 14:59:04 -0800

    backwards compatible csm fetching (2)

--- openquake/hme/utils/io/source_reader.py
+++ openquake/hme/utils/io/source_reader.py
@@ -8,13 +8,17 @@ from openquake.baselib.general import AccumDict
 
 from openquake.commonlib import datastore
 from openquake.commonlib.readinput import get_params
-from openquake.hazardlib.source_group import read_csm
 from openquake.engine.engine import create_jobs, run_jobs
 
 from openquake.hazardlib.gsim_lt import GsimLogicTree
 
 from openquake.hme.utils.utils import _get_class_name, breakpoint
 
+try:
+    from openquake.hazardlib.source_group import read_csm
+    csm_new_flag = True
+except ImportError:
+    csm_new_flag = False
 
 def csm_from_job_ini(job_ini, get_gsim_lt: bool = False):
     if not isinstance(job_ini, dict) and os.path.isfile(job_ini):
@@ -37,10 +41,10 @@ def csm_from_job_ini(job_ini, get_gsim_lt: bool = False):
     logging.debug("\tran job")
     logging.debug("getting csm from dstore")
     with job, datastore.read(job.calc_id) as dstore:
-        try: # older OQ
-            csm = dstore['_csm']
-        except: # newer OQ
+        if csm_new_flag:
             csm = read_csm(dstore)
+        else: # older OQ
+            csm = dstore['_csm']
         sources = csm.get_sources()
         logging.debug("\tgot csm from dstore")
 
